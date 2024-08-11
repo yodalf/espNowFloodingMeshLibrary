0ea8bdcc68db100f2db991e54236afae1c710ed7
Real Ouellet <yodalf@users.noreply.github.com>
2024-08-11 11:21:43 -0400

    Update EspNowFloodingMesh.cpp

--- EspNowFloodingMesh.cpp
+++ EspNowFloodingMesh.cpp
@@ -708,7 +708,7 @@ bool forwardMsg(const uint8_t *data, int len) {
 }
 
 
-uint32_t sendMsg(uint8_t* msg, unsigned int size, int ttl, int msgId, void *ptr) {
+uint32_t sendMsg(uint8_t* msg, int size, int ttl, int msgId, void *ptr) {
   uint32_t ret=0;
   if(size>=sizeof(struct mesh_secred_part)) {
     #ifdef DEBUG_PRINTS
